5f78727daf668d4cd39d887892a12893fc492a63
Stubilis <estiichy@gmail.com>
2024-05-20 00:05:24 +0200

    added download button to pdf

--- FlavorCart/Controllers/PDFCreatorController.cs
+++ FlavorCart/Controllers/PDFCreatorController.cs
@@ -3,6 +3,9 @@ using DinkToPdf.Contracts;
 using Microsoft.AspNetCore.Mvc;
 using FlavorCart.Utility;
 using System.IO;
+using FlavorCart.Models;
+using FlavorCart.Repositories;
+using Firestore.Controllers;
 
 namespace PDF_Generator.Controllers
 {
@@ -10,6 +13,8 @@ namespace PDF_Generator.Controllers
     [ApiController]
     public class PdfCreatorController : ControllerBase
     {
+        private readonly ListsRepository _listsRepository = new();
+        private readonly ArticleRepository _articleRepository = new();
         private IConverter _converter;
 
         public PdfCreatorController(IConverter converter)
@@ -18,8 +23,33 @@ namespace PDF_Generator.Controllers
         }
 
         [HttpGet]
-        public IActionResult CreatePDF()
+        [Route("{id}")]
+        public async Task<IActionResult> CreatePDFAsync(string  id)
         {
+    
+            //Get the data from the database
+            Lists lists = new Lists()
+            {
+                Id = id
+            };
+            var _list = await _listsRepository.GetAsync(lists);
+            if (_list == null)
+            {
+                return NotFound();
+            }
+            //Get the list of articles from the list
+            List <Article> articles = new List<Article>();
+            foreach (var item in _list.ArticleList)
+            {
+                Article article = new Article()
+                {
+                    Id = item.ArticleId
+                };
+                articles.Add(await _articleRepository.GetAsync(article));
+                
+            }
+                     
+
             var globalSettings = new GlobalSettings
             {
                 ColorMode = ColorMode.Color,
@@ -27,13 +57,13 @@ namespace PDF_Generator.Controllers
                 PaperSize = PaperKind.A4,
                 Margins = new MarginSettings { Top = 10 },
                 DocumentTitle = "PDF Report",
-                Out = @"C:\PDFCreator\Employee_Report.pdf"  //USE THIS PROPERTY TO SAVE PDF TO A PROVIDED LOCATION
+               // Out = @"C:\PDFCreator\"+_list.Name+".pdf"  //USE THIS PROPERTY TO SAVE PDF TO A PROVIDED LOCATION
             };
 
             var objectSettings = new ObjectSettings
             {
                 PagesCount = true,
-                HtmlContent = TemplateGenerator.GetHTMLString(),
+                HtmlContent = TemplateGenerator.GetHTMLString(_list, articles),
                // Page = "https://code-maze.com/", //USE THIS PROPERTY TO GENERATE PDF CONTENT FROM AN HTML PAGE
                 WebSettings = { DefaultEncoding = "utf-8", UserStyleSheet = Path.Combine(Directory.GetCurrentDirectory(), "assets", "styles.css") },
                 HeaderSettings = { FontName = "Arial", FontSize = 9, Right = "Page [page] of [toPage]", Line = true },
@@ -46,12 +76,12 @@ namespace PDF_Generator.Controllers
                 Objects = { objectSettings }
             };
 
-            _converter.Convert(pdf);// IF WE USE Out PROPERTY IN THE GlobalSettings CLASS, THIS IS ENOUGH FOR CONVERSION
+            //_converter.Convert(pdf);// IF WE USE Out PROPERTY IN THE GlobalSettings CLASS, THIS IS ENOUGH FOR CONVERSION
 
-            //var file = _converter.Convert(pdf);
+            var file = _converter.Convert(pdf);
 
-            return Ok("Successfully created PDF document.");
-            //return File(file, "application/pdf", "EmployeeReport.pdf");
+            //return Ok("Successfully created PDF document.");
+            return File(file, "application/pdf", _list.Name+".pdf");
             //return File(file, "application/pdf");
         }
     }
--- FlavorCart/Utility/TemplateGenerator.cs
+++ FlavorCart/Utility/TemplateGenerator.cs
@@ -1,39 +1,45 @@
-﻿using System.Text;
+﻿using FlavorCart.Models;
+using FlavorCart.Repositories;
+using FlavorCart.Enums;
+using System.Text;
 
 namespace FlavorCart.Utility
 {
     public class TemplateGenerator
     {
-        public static string GetHTMLString()
+                
+        public static string GetHTMLString(Lists list, List<Article> articles)
         {
-            var employees = DataStorage.GetTestLists();
+
             var sb = new StringBuilder();
-            sb.Append(@"
-                        <html>
-                            <head>
-                            </head>
-                            <body>
-                                <div class='header'><h1>This is the generated PDF report!!!</h1></div>
-                                <table align='center'>
-                                    <tr>
-                                        <th>Name</th>
-                                        <th>User</th>
-                                        <th>Date</th>
-                                        <th>Price</th>
-                                    </tr>");
-            foreach (var emp in employees)
+            sb.AppendFormat(@"
+                            <html>
+                                <head>
+                                </head>
+                                <body>
+                                    <div class='header'><h1>{0}</h1></div>
+                                    <table align='center'>
+                                        <tr>
+                                            <th>Name</th>
+                                            <th>Amount</th>
+                                            <th>Unit</th>
+                                            <th>AvgPrice</th>
+                                        </tr>", list.Name);
+
+            foreach (var item in list.ArticleList)
             {
+                var article = articles.Find(a => a.Id == item.ArticleId);
                 sb.AppendFormat(@"<tr>
-                                    <td>{0}</td>
-                                    <td>{1}</td>
-                                    <td>{2}</td>
-                                    <td>{3}</td>
-                                  </tr>", emp.Name, emp.UserId, emp.CreationDate, emp.IsPublic);
+                                        <td>{0}</td>
+                                        <td>{1}</td>
+                                        <td>{2}</td>
+                                        <td>{3}</td>
+                                      </tr>", article.Name,item.Amount,item.Unit, article.AveragePrice);
             }
             sb.Append(@"
-                                </table>
-                            </body>
-                        </html>");
+                                    </table>
+                                </body>
+                            </html>");
             return sb.ToString();
         }
     }
