efb55186197370f1bcef9798985d13c5a8e2d897
Stubilis <estiichy@gmail.com>
2024-05-21 17:26:06 +0200

    added method to add the articlelist from recipe to lists

--- FlavorCart/Controllers/ListsFirestoreController.cs
+++ FlavorCart/Controllers/ListsFirestoreController.cs
@@ -11,6 +11,7 @@ public class ListsFirestoreController : ControllerBase
     private readonly ILogger<UserFirestoreController> _logger;
     // This should be injected - This is only an example
     private readonly ListsRepository _listsRepository = new();
+    private readonly RecipeRepository _recipeRepository = new();
     private UserTokenController _usertokenFirestoreController;
     public ListsFirestoreController(ILogger<UserFirestoreController> logger)
     {
@@ -105,6 +106,61 @@ public class ListsFirestoreController : ControllerBase
         }
         return BadRequest("Invalid token");
     }
+    //TODO Add recipe to list
+    [HttpPut]
+    [Route("addRecipe/{recipeId}")]
+    public async Task<ActionResult<User>> AddRecipeToListAsync(string recipeId, Lists lists)
+    {
+        try
+        {
+            var ok = await _usertokenFirestoreController.Verify(Request.Headers["Authorization"].ToString().Remove(0, 7));
+            if (ok != null)
+            {
+                
+                    Recipe recipe = _recipeRepository.GetAsync(new Recipe() { Id = recipeId }).Result!;
+                
+               
+                //Compare the article list of the recipe with the article list of the list
+                if (recipe != null && recipe.ArticleList.Count > 0)
+                {
+                    bool found;
+                    foreach (var item2 in recipe.ArticleList)
+                    {
+                        found = false;
+                        foreach (var item in lists.ArticleList)
+                        {
+                            if (item.ArticleId == item2.ArticleId)
+                            {
+                                if (item.IsActive)
+                                {
+                                    item.Amount += item2.Amount;
+                                }
+                                else
+                                {
+                                    item.IsActive = true;
+                                    item.Amount = item2.Amount;
+                                }
+                                found = true;
+                                break;
+                            }
+                        }
+                        if (!found)
+                        {
+                            lists.ArticleList.Add(item2);
+                        }
+                    }
+                                    
+                }
+                return Ok(await _listsRepository.UpdateAsync(lists));
+            }
+        }
+        catch (Exception e)
+        {
+            return BadRequest("Missing token");
+        }
+        return BadRequest("Invalid token");
+        
+    }   
 
     [HttpDelete]
     [Route("{id}")]
