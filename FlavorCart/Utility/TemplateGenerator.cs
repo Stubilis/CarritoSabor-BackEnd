491d7e7ed3a0a68bad30324a6f0b4e8a9b91eefb
Stubilis <estiichy@gmail.com>
2024-06-03 19:16:57 +0200

    Added styles to the pdfs

--- FlavorCart/Utility/TemplateGenerator.cs
+++ FlavorCart/Utility/TemplateGenerator.cs
@@ -8,13 +8,12 @@ namespace FlavorCart.Utility
     public class TemplateGenerator
     {
 
-        public static string ListsGetHTMLString(Lists list, List<Article> articles, string logoPath,string username)
+        public static string ListsGetHTMLString(Lists list, List<Article> articles, string logoPath, string username)
         {
-            //Format average price to 2 decimals
-           
+            //TO DO: split the list each 35 items to avoid overflow in the pdf
 
-            var sb = new StringBuilder();
-            sb.AppendFormat(@"
+                var sb = new StringBuilder();
+                sb.AppendFormat(@"
                                 <html>
                                     <head>
                                         <div class='logo'>
@@ -31,34 +30,34 @@ namespace FlavorCart.Utility
                                                 <th>Nombre</th>
                                                 <th>Cantidad</th> 
                                                 <th>Precio</th>
-                                            </tr>", logoPath,username, list.Name);
+                                            </tr>", logoPath, username, list.Name);
 
-            foreach (var item in list.ArticleList)
-            {
-                var article = articles.Find(a => a.Id == item.ArticleId);
-                if (item.IsActive)
+                foreach (var item in list.ArticleList)
                 {
-                    sb.AppendFormat(@"<tr>
+                    var article = articles.Find(a => a.Id == item.ArticleId);
+                    if (item.IsActive)
+                    {
+                        sb.AppendFormat(@"<tr>
                                             <td><input type=""checkbox""></td>
                                             <td>{0}</td>
                                             <td>{1} {2}</td>
                                             <td>{3} €</td>
                                           </tr>", article.Name, item.Amount, item.Unit, Math.Round(article.AveragePrice, 2));
-                }
-                else
-                {
-                    sb.AppendFormat(@"<tr>
+                    }
+                    else
+                    {
+                        sb.AppendFormat(@"<tr>
                                             <td><input type=""checkbox"" checked></td>
                                             <td>{0}</td>
                                             <td>{1} {2}</td>
                                             <td>{3} €</td>
                                           </tr>", article.Name, item.Amount, item.Unit, Math.Round(article.AveragePrice, 2));
+                    }
                 }
-            }
-            sb.AppendFormat(@" 
+                sb.AppendFormat(@" 
                                       <tr>
-                                        <td></td>
-                                        <td></td>
+                                        <th></th>
+                                        <th></th>
                                         <th>Total</th>
                                         <td class ='total'>{0} €</td>
                                         </tr>
@@ -66,9 +65,12 @@ namespace FlavorCart.Utility
                                        </div>
                                         
                                     </body>
-                                </html>", Math.Round(list.TotalPrice,2));
-            return sb.ToString();
+                                </html>", Math.Round(list.TotalPrice, 2));
+                return sb.ToString();
+         
         }
+
+        
 
         public static string RecipeGetHTMLString(Recipe recipe, List<Article> articles)
         {
@@ -101,7 +103,7 @@ namespace FlavorCart.Utility
                                             <td>{1}</td>
                                             <td>{2}</td>
                                             <td>{3}</td>
-                                          </tr>", article.Name, item.Amount, item.Unit, article.AveragePrice);
+                                          </tr>", article.Name, item.Amount, item.Unit, Math.Round(article.AveragePrice, 2));
                 }
                 else
                 {
@@ -111,7 +113,7 @@ namespace FlavorCart.Utility
                                             <td>{1}</td>
                                             <td>{2}</td>
                                             <td>{3}</td>
-                                          </tr>", article.Name, item.Amount, item.Unit, article.AveragePrice);
+                                          </tr>", article.Name, item.Amount, item.Unit, Math.Round(article.AveragePrice, 2));
                 }
             }
                
